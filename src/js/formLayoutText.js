a8fb5832a614f83f6245e265f2d924e968064f5a
BORAMLEE <boram1073@gmail.com>
2021-04-07 18:07:29 +0900

    feat : semantic ui react form element 부분 작업 추가(진행 중)

--- src/js/formLayoutText.js
+++ src/js/formLayoutText.js
@@ -5,23 +5,14 @@ import { List } from 'semantic-ui-react';
 
 const formExample = () => {
 
-    // const imgError = (e) => {
-    //     e.target.src = "../img/fin.png";
-    // };
-
     const guideTxt1 = [
         {id:1, msg:(
             <span>
                 Form 요소들을 감싸는 레이아웃으로 <strong className="txt-color01">form 또는 div 태그에 꼭 "ui form" 클래스를 사용</strong>하고, 그 내부에 요소들을 넣는다.
             </span>
         )},
-        {id:2, msg:(
-            <span>
-                <img src="../img/logo.svg" onError="../img/fin.png" alt="test" />
-            </span>
-        )},
-        {id:3, msg:"브라우저 지원 범위 : 최신 chrome, Edge, firefox, opera 브라우저 / IE 11 ~"},
-        {id:4, msg:"기본 레이아웃 이외의 배치가 필요한 화면은 퍼블리셔에게 문의한다."},
+        {id:2, msg:"브라우저 지원 범위 : 최신 chrome, Edge, firefox, opera 브라우저 / IE 11 ~"},
+        {id:3, msg:"기본 레이아웃 이외의 배치가 필요한 화면은 퍼블리셔에게 문의한다."},
         
     ];
 
--- src/js/formLayoutVisual.js
+++ src/js/formLayoutVisual.js
@@ -1,23 +1,17 @@
 import React from 'react'
 import classnames from 'classnames'
-import { Item, List } from 'semantic-ui-react';
+import { Item, List, Form, Input, TextArea, Button, Select, Radio, Checkbox, TextArea } from 'semantic-ui-react';
 
 const formVisualExample = () => {
-
-    const formVisual = (newClassName) => (
-        <div className={classnames("flex-layout" , newClassName)}>
-            <div className="box">1</div>
-            <div className="box">2</div>
-            <div className="box">3</div>
-            <div className="box">4</div>
-            <div className="box">5</div>
-        </div>
-    );
-
-    const formGuideText = (formGuide1, formGuide2) => {
+    /*------------------------------- guide text -------------------------------*/
+    const formGuideText = (formGuide1, formGuide2, formGuide3, formGuide4, formGuide5, formGuide6) => {
         const guideTxt1 = [
             {id:1, msg:formGuide1},
             {id:2, msg:formGuide2},
+            {id:3, msg:formGuide3},
+            {id:4, msg:formGuide4},
+            {id:5, msg:formGuide5},
+            {id:6, msg:formGuide6},
         ];
     
         const guideList = guideTxt1.map((guideTxt, index) => <List.Item key={index}>{guideTxt.msg}</List.Item>);
@@ -26,61 +20,162 @@ const formVisualExample = () => {
         
     };
 
+    /*------------------------------- form elements -------------------------------*/
+    const innerBoxStyles= {
+        margin:"0 10px",
+        padding:"20px",
+        border:"1px solid #fff"
+    };
+    const formField = (formElement) => (
+        <Form>
+            {formElement}
+        </Form>
+    );
+    
+    /* default form field(depth2) */
+    const formFieldEqual = () => (
+        <Form.Group widths='equal'>
+          <Form.Input fluid label='First name' placeholder='EX.레오나르도' />
+          <Form.Input fluid label='Middle name' placeholder='EX.영철' />
+          <Form.Input fluid label='Last name' placeholder='EX.손' />
+        </Form.Group>
+    );
+    const formFieldEqual2 = () => (
+        <Form.Group widths='3'>
+          <Form.Input fluid label='First name' placeholder='EX.레오나르도' />
+          <Form.Input fluid label='Last name' placeholder='EX.손' />
+        </Form.Group>
+    );
+    const formFields = () => {
+        const formFieldsList = [
+            {id:1, tag:(
+                <Form.Group>
+                <Form.Input placeholder='1 wide' width={1} />
+                <Form.Input placeholder='1 wide' width={1} />
+                <Form.Input placeholder='1 wide' width={1} />
+                <Form.Input placeholder='1 wide' width={1} />
+                <Form.Input placeholder='1 wide' width={1} />
+                <Form.Input placeholder='1 wide' width={1} />
+                <Form.Input placeholder='1 wide' width={1} />
+                <Form.Input placeholder='1 wide' width={1} />
+                <Form.Input placeholder='1 wide' width={1} />
+                <Form.Input placeholder='1 wide' width={1} />
+                <Form.Input placeholder='1 wide' width={1} />
+                <Form.Input placeholder='1 wide' width={1} />
+                <Form.Input placeholder='1 wide' width={1} />
+                <Form.Input placeholder='1 wide' width={1} />
+                <Form.Input placeholder='1 wide' width={1} />
+                <Form.Input placeholder='1 wide' width={1} />
+                </Form.Group>
+            )},
+            {id:2, tag:(
+                <Form.Group>
+                <Form.Input placeholder='1 wide' width={4} />
+                <Form.Input placeholder='1 wide' width={4} />
+                <Form.Input placeholder='1 wide' width={4} />
+                <Form.Input placeholder='1 wide' width={4} />
+                </Form.Group>
+            )},
+            {id:3, tag:(
+                <Form.Group>
+                <Form.Input placeholder='1 wide' width={2} />
+                <Form.Input placeholder='1 wide' width={12} />
+                <Form.Input placeholder='1 wide' width={2} />
+                </Form.Group>
+            )},
+            {id:4, tag:(
+                <Form.Group>
+                <Form.Input placeholder='1 wide' width={8} />
+                <Form.Input placeholder='1 wide' width={5} />
+                <Form.Input placeholder='1 wide' width={3} />
+                </Form.Group>
+            )},
+        ];
+    
+        const formFields = formFieldsList.map((formFieldsTag, index) => <List.Item key={index}>{formFieldsTag.tag}</List.Item>);
+    
+        return <List>{formFields}</List>;
+    };
+    const formVisualDetailInner1 = () => {
+
+        const detailInnerList = [
+            {
+                header:'균등 필드',
+                description:'한 row에 10개 이하 field이면서 모두 일정한 너비를 가지는 경우 : 해당 필드를 "<Form.Group widths="equal">" 폼 그룹 태그로 감싸서 사용하며, 1개일 경우엔 부모 태그 생략가능.(field가 3개에서 2개로 줄어들 경우 빈 공간을 메꿔준다.)',
+                extra:formField(formFieldEqual()),
+            },
+            {
+                header:'균등 필드 - 필드 개수 직접 지정(밑에 예시는 필드 개수를 3개로 지정한 상태)',
+                description:'한 row에 10개 이하 field이면서 모두 일정한 너비를 가지는 경우 : 해당 필드를 "<Form.Group widths="필드개수">"폼 그룹 태그로 감싸서 사용하며, 1개일 경우엔 부모 태그 생략가능.(field가 3개에서 2개로 줄어들 경우 빈 공간을 안 메꿔준다.)',
+                extra:formField(formFieldEqual2()),
+            },
+            {
+                header:'필드 - 필드 너비 직접 지정',
+                description:'한 row에 10개 초과 또는 10이하이면서 개별 너비를 지정하고싶은 경우 :"<Form.Group>" 폼 그룹 태그로 감싼 후, 하위 field 태그에서 개별로 너비값 조정. 하위 field 태그에선 1 ~ 12 범위로 너비 조정 가능 하위 태그 클래스는"<Form.Input width={필드너비}>"에서 width 속성에 숫자로 입력하여 사용한다.',
+                extra:formField(formFields()),
+            },
+       ];
+
+       return <Item.Group items={detailInnerList} style={innerBoxStyles} />
+    };
+
+
+    /* inline form field(depth2) */
+    const checkboxInline = () => (
+        <Form.Group inline>
+            <label>과일</label>
+          <Form.Input fluid label='First name' placeholder='EX.레오나르도' />
+          <Form.Input fluid label='Middle name' placeholder='EX.영철' />
+          <Form.Input fluid label='Last name' placeholder='EX.손' />
+        </Form.Group>
+    );
+    const formVisualDetailInner2 = () => {
+
+        const detailInnerList = [
+            {
+                header:'checkbox',
+                description:'',
+                extra:formField(checkboxInline()),
+            },
+            {
+                header:'radio button',
+                description:'',
+                extra:formField(formFieldEqual2()),
+            },
+            {
+                header:'text field',
+                description:'',
+                extra:formField(formFields()),
+            },
+       ];
+
+       return <Item.Group items={detailInnerList} style={innerBoxStyles} />
+    };
+
+
+    //form elements layout
     const formVisualDetail = [
         {
             header:'Default',
             description:formGuideText(
-                '폼 필드 레이아웃 기본 정렬 형태 -> 레이블: 블럭요소(단독으로 한 줄 차지) / 필드: 블럭요소 (개수를 지정하지 않으면 단독으로 한 줄 차지)',
-                (
-                    <span>
-                        한 row에 10개 이하 field이면서 모두 일정한 너비를 가지는 경우 : 해당 필드를 <strong className="txt-color01">"필드개수 + fields"</strong> 클래스를 적용한 div 태그로 감싸서 사용하며, 1개일 경우엔 부모 태그 생략가능.<strong className="txt-color01">(field가 5개에서 4개로 줄어들 경우 빈 공간을 안 메꿔준다.)</strong>
-                    </span>
-                ),
+                '- 폼 필드 레이아웃 기본 정렬 형태 -> 레이블: 블럭요소(단독으로 한 줄 차지) / 필드: 블럭요소 (개수를 지정하지 않으면 단독으로 한 줄 차지)',
+                '- 필드: 블럭요소 (개수를 지정하지 않으면 단독으로 한 줄 차지)',
+                '- 자세한 예시는 하단 참고'
             ),
-            extra:formVisual(""),
+            extra:formVisualDetailInner1(),
+            
         },
         {
-            header:'수평 정렬 - 좌측 정렬',
-            description:'부모 태그에 "justify-start" 클래스 추가. 모든 자식 태그가 여백 없이 좌측으로 정렬된다.(flex-layout을 지정하면 기본적으로 적용된다.)',
-            extra:formVisual("justify-start"),
-        },
-        {
-            header:'수평 정렬 - 우측 정렬',
-            description:'부모 태그에 "justify-end" 클래스 추가. 모든 자식 태그가 여백 없이 우측으로 정렬된다.',
-            extra:formVisual("justify-end"),
-        },
-        {
-            header:'수평 정렬 - 가운데 정렬',
-            description:'부모 태그에 "justify-center" 클래스 추가. 모든 자식 태그가 여백 없이 가운데로 정렬된다.',
-            extra:formVisual("justify-center"),
-        },
-        {
-            header:'수평 정렬 - 양측 균등 정렬',
-            description:'부모 태그에 "justify-sb" 클래스 추가.  1, 5번 자식이 여백 없이 양 사이드에 바짝 붙고, 나머지 자식들은 비율에 맞게 여백을 가지며 정렬된다.',
-            extra:formVisual("justify-sb"),
-        },
-        {
-            header:'수직 정렬 - 상단 정렬',
-            description:'부모 태그에 "align-start" 클래스 추가. 모든 자식 태그가 여백 없이 상단으로 정렬된다.(flex-layout을 지정하면 기본적으로 적용된다.)',
-            extra:formVisual("align-start"),
-        },
-        {
-            header:'수직 정렬 - 하단 정렬',
-            description:'부모 태그에 "align-end" 클래스 추가. 모든 자식 태그가 여백 없이 하단으로 정렬된다.',
-            extra:formVisual("align-end"),
-        },
-        {
-            header:'수직 정렬 - 가운데 정렬',
-            description:'부모 태그에 "align-center" 클래스 추가. 모든 자식 태그가 여백 없이 가운데로 정렬된다.',
-            extra:formVisual("align-center"),
-        },
-        {
-            header:'수직 정렬 - 스트레치',
-            description:'부모 태그에 "align-stretch" 클래스 추가. 자식 태그의 높이가 \'auto\'로서 부모 크기에 맞게 균등하게 늘어난다.(5개중 1개의 높이가 길 경우, 그 것에 맞춰 나머지가 늘어난다. 하지만, 부모보다 높이가 줄어들 경우 해당 항목만 줄어든다. 고정 높이가 있을 경우 무시된다.)',
-            extra:formVisual("align-stretch"),
+            header:'Inline 정렬',
+            description:formGuideText(
+                '- 폼 필드 레이아웃 인라인 정렬 형태(한 줄에 표현되는 형태) -> 레이블: 인라인요소(레이블 크기만큼 차지) / 필드: 인라인요소 (필드 크기만큼 차지)',
+                '- 자세한 예시는 하단 참고'
+            ),
+            extra:formVisualDetailInner2(),
+            
         },
     ];
-
+        
     return <Item.Group items={formVisualDetail} />
 
 };
